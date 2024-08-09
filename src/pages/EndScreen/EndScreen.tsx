fe6f63ed9c72b8ec4758f51a3543c7b3ff334453
zylic <tomas.rasala@gmail.com>
2024-08-09 09:09:30 +0300

    End Screen started off

---
+++ src/pages/EndScreen/EndScreen.tsx
@@ -0,0 +1,12 @@
+import "./EndScreen.css";
+
+const EndScreen = () => {
+    return (
+        <div className="screen end-screen">
+            <h1 className="end-screen-title">Pelin Loppu</h1>
+            <button className="end-screen-restart-button">Aloita Uudestaan</button>
+        </div>
+    )
+}
+
+export default EndScreen;
