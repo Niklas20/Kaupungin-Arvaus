848cdfd7a137dc09607db4c5aaad8a01e97f77c5
Niklas Virtanen <93010689+Niklas20@users.noreply.github.com>
2024-08-09 11:46:33 +0300

    added layout component to the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
 import Router from "./Router"
 import "./App.css"
-import { useNavigate } from "react-router-dom";
 
 const App = () => {
-  const navigate = useNavigate();
-
-    const handleExitButtonClick = () => {
-        navigate("/")
-    }
-
-    const handleInfoButtonClick = () => {
-        navigate("/")
-    }
-
-    const handleHelpButtonClick = () => {
-      navigate("/")
-    }
-
   return (
     <>
       <div className="container">
         <Router />
-
-        <button
-          className="exit-button"
-          onClick={handleExitButtonClick}
-        > Exit
-        </button>
-
-        <button
-          className="info-button"
-          onClick={handleInfoButtonClick}
-        > Info
-        </button>
-
-        <button
-          className="help-button"
-          onClick={handleHelpButtonClick}
-        > Help
-        </button>
-
       </div>
     </>
   )
