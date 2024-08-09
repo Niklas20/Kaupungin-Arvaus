9501ebba5d7dd949b9ce6488fe260b2441d7ea30
Niklas Virtanen <93010689+Niklas20@users.noreply.github.com>
2024-08-09 09:02:02 +0300

    chore: Update InfoScreen component styling

--- src/pages/InfoScreen/InfoScreen.tsx
+++ src/pages/InfoScreen/InfoScreen.tsx
@@ -3,8 +3,9 @@ import "./InfoScreen.css";
 const InfoScreen = () => {
     return (
         <div className="screen info-screen">
-            <h1>Ohjeet</h1>
-            <p>Tervetuloa pelaamaan kaupungin arvaus peliä!</p>
+            <h1 className="info-screen-title">Ohjeet</h1>
+            <p className="info-screen-text-1">Tervetuloa pelaamaan kaupungin arvaus peliä!</p>
+            <button className="info-screen-button">Aloita peli</button>
         </div>
     )
 }
