596ad783baff17cfb339b8cee90eb6db6493862b
zylic <tomas.rasala@gmail.com>
2024-08-09 11:38:23 +0300

    button bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import Router from "./Router"
 import "./App.css"
+import { useNavigate } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
+
+    const handleExitButtonClick = () => {
+        navigate("/")
+    }
+
+    const handleInfoButtonClick = () => {
+        navigate("/")
+    }
+
+    const handleHelpButtonClick = () => {
+      navigate("/")
+    }
+
   return (
     <>
       <div className="container">
         <Router />
+
+        <button
+          className="exit-button"
+          onClick={handleExitButtonClick}
+        > Exit
+        </button>
+
+        <button
+          className="info-button"
+          onClick={handleInfoButtonClick}
+        > Info
+        </button>
+
+        <button
+          className="help-button"
+          onClick={handleHelpButtonClick}
+        > Help
+        </button>
+
       </div>
     </>
   )
